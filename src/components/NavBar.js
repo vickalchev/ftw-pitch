34327d1080d8f2e4aceb6e8901b7166a810def51
Vic Kalchev <vic@pivotup.co>
2022-08-14 14:59:24 -0400

    updated navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Logo from '../assets/FTW Pitch Logo.png';
+import {
+    container,
+    logo,
+    navMenu,
+    navList,
+    menuItem,
+
+} from './navBar.module.css';
+
+const NavBar = () => {
+    return (
+        <div className={container}>
+           <Link to='/'>
+                <img 
+                    className={logo}
+                    src={Logo} 
+                    alt='FTW Pitch Logo'
+                />
+            </Link>
+
+            <nav className={navMenu}>
+                <ul className={navList}>
+                    <li className={menuItem}>
+                        How It Works
+                    </li>
+                    <li className={menuItem}>
+                        Let's Chat
+                    </li>
+                    <li className={menuItem}>
+                        Team
+                    </li>
+                    <li className={menuItem}>
+                        FTW.DAO
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+ export default NavBar;
