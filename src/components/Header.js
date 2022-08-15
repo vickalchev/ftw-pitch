2871ac3fdf6bdaaa2afc43168fb0e0cd67a6a909
Vic Kalchev <vic@pivotup.co>
2022-08-15 17:54:40 -0400

    Added the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ import {
     circle3,
     h1,
     h2,
+    floodlightIcon,
     howItWorksBtn,
 } from './header.module.css';
 
@@ -17,7 +18,7 @@ const Header = () => {
             <div className = {circle2} />
             <div className = {circle3} />
             <h1 className= {h1}>VCs helping founders fundraise</h1>
-            <h2 className={h2}>Fundraising can feel like a dark forest with no path in sight.We've got the map and the flood lights.</h2> <span></span>
+            <h2 className={h2}>Fundraising can feel like a dark forest with no path in sight. We've got the map and the flood lights.</h2> <span className={floodlightIcon}></span>
             <button className = {howItWorksBtn}>How it works</button>
         
         </div>
