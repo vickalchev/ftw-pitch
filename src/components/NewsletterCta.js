b592a0ff5a6d1c74eaaa2f351f31357ea719be55
Vic Kalchev <vic@pivotup.co>
2022-08-21 19:30:38 -0400

    Improved styling of the Newsletter component

---
+++ src/components/NewsletterCta.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import NewsletterCTAImg from '../images/NewsletterCTAImg.png';
+import {
+    newsletterCtaBkg,
+    container,
+    newsletter,
+    magnetTitle,
+    ctaBtn,
+    newsletterCTAImg,
+    h1NewsletterCta,
+    h2NewsletterCta,
+    magnetWrapper,
+    leftCol,
+    rightCol,
+
+
+} from './newsletterCTA.module.css';
+
+const NewsletterCTA = () => {
+    return (
+        <div className={newsletterCtaBkg}>
+            {/* 👆 This is the background div that bleeds. */}
+            <div className={container}>
+                {/* 👆 This div ensure that the content width is at 80% of the vw. */}
+                <div className={leftCol}>
+                    <h1 className={h1NewsletterCta}>
+                        Sign up for our
+                        <br/>
+                        <span className={newsletter}>newsletter</span>
+                    </h1>
+                    <div className={magnetWrapper}>
+                        <h2 className={h2NewsletterCta}>and get a free copy of
+                            <br/>
+                            <span className={magnetTitle}>6 Deadly Traps of Building a Pitch Deck</span>
+                        </h2>
+                    </div>
+                    <button className={ctaBtn}>Get It Now!</button>
+                </div>
+                <div className={rightCol} >
+                        <img
+                            className={newsletterCTAImg}
+                            src={NewsletterCTAImg}
+                            alt='person with computer'
+
+                            />
+                </div>
+            </div>
+                {/* We want to keep the button outside of the div above. The bottomSection div use display:flex to align the heading and image in a row. We don't want the button to also be displayed in that row on bigger screens. This is why, we keep it separately. */}
+
+            </div>
+    )
+}
+
+export default NewsletterCTA;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Header from '../components/Header';
 import Process from '../components/Process';
 import CTA1 from '../components/CTA1';
 import Testimonials from '../components/Testimonials';
+import NewsletterCTA from '../components/NewsletterCta';
 
 const IndexPage = () => {
   return (
@@ -14,6 +15,7 @@ const IndexPage = () => {
       <Process />
       <CTA1 />
       <Testimonials />
+      <NewsletterCTA />
       
 
     </main>
