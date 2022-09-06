8eb7595e7544d311aa0312d77f8ee273d219edae
Vic Kalchev <vic@pivotup.co>
2022-09-06 16:56:20 -0400

    Set up Pricing component

---
+++ src/components/Pricing.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    container,
+    imgContainer,
+    copyContainer,
+} from './PricingStyle.css';
+
+
+const Pricing = () => {
+    return (
+        <div className={container}>
+            <div className={imgContainer}>
+
+            </div>
+
+            <div className={copyContainer}>
+
+            </div>
+        </div>
+    )
+}
+
+export default Pricing;
